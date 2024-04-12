1e6d7d9f3b7494d49110a8dd0365892c5a2d11d3
ktvng <35818433+ktvng@users.noreply.github.com>
2024-04-12 16:21:25 -0400

    disable local storage to test site gen

--- docs/components/InputBox.vue
+++ docs/components/InputBox.vue
@@ -2,10 +2,10 @@
 const props = defineProps(['answer', 'name'])
 
 function storeChange() {
-  localStorage.setItem(props.name, document.querySelector("#" + props.name).value)
+  // localStorage.setItem(props.name, document.querySelector("#" + props.name).value)
 }
 
-let lastResponse = localStorage.getItem(props.name) ?? ""
+// let lastResponse = localStorage.getItem(props.name) ?? ""
 </script>
 <template>
   <div>
@@ -17,7 +17,7 @@ let lastResponse = localStorage.getItem(props.name) ?? ""
         border-width: .11em; border-color: #5468ff; border-style: solid; width: 32em;
         font-size: 14px; font-family: var(--vp-font-family-base);"
       @change="storeChange"
-      placeholder="Your answer: "></input>
+      placeholder="Your answer: " />
   </div>
 </template>
 <script>
